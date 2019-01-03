c670e92148bd29da646c8876764cffd97bc0f85e
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-03 16:43:07 -0500

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,14 @@
 # Create your costume_stores migration here
-class CreateCostumeStores <ActiveRecord::MIgration[5.1]
+class CreateCostumeStores <ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
       t.string :costume_inventory
-      t.integer :number_of_employees
-      t.boolean :in_business
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.timestamp :opening_time
       t.timestamp :closing_time
     end
+  end
 end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,14 +1,15 @@
 # Create your haunted_houses migration here
-class HauntedHouse < ActiveRecord::Migration[5.1]
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
       t.string :theme
       t.integer :price
-      t.string :family_friendly
+      t.boolean :family_friendly
       t.timestamp :opening_date
       t.timestamp :closing_date
-      t.string :long_description
+      t.text :description
     end  
+  end
 end
