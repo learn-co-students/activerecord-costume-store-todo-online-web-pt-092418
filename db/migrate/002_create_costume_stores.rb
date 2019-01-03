a8648ef75910bdafb6a0fd63120c82cd72624dcf
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-03 16:05:28 -0500

    Interim checkin

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CreateCostumeStores <ActiveRecord::MIgration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :in_business
+      t.timestamp :opening_time
+      t.timestamp :closing_time
+    end
+end
